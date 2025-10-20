abfb6f04d7bb696c1cd87a022e3743f6754cd1d4
conorgibbons147 <hjd3db@virginia.edu>
2025-10-20 15:07:25 -0400

    saving class changes

--- receive-message.py
+++ receive-message.py
@@ -2,19 +2,19 @@ import os
 import boto3
 import time
 
-queue_url = "https://sqs.us-east-1.amazonaws.com/440848399208/xxxxx"
+queue_url = "https://sqs.us-east-1.amazonaws.com/440848399208/hjd3db"
 sqs = boto3.client('sqs')
 
-# def delete_message(queue_url, receipt_handle):
-#     try:
-#         response = sqs.delete_message(
-#             QueueUrl=queue_url,
-#             ReceiptHandle=receipt_handle
-#         )
-#         print(f"Response: {response}")
-#     except Exception as e:
-#         print(f"Error deleting message: {e}")
-#         raise e
+def delete_message(queue_url, receipt_handle):
+    try:
+        response = sqs.delete_message(
+            QueueUrl=queue_url,
+            ReceiptHandle=receipt_handle
+        )
+        #print(f"Response: {response}")
+    except Exception as e:
+        #print(f"Error deleting message: {e}")
+        raise e
 
 def get_message(queue_url):
     # try to get any messages with message-attributes from SQS queue:
@@ -25,18 +25,19 @@ def get_message(queue_url):
             MaxNumberOfMessages=1,
             VisibilityTimeout=60,
             MessageAttributeNames=['All'],
-            WaitTimeSeconds=30
+            WaitTimeSeconds=10
         )
-#         receipt_handle = response['Messages'][0]['ReceiptHandle']
-#         delete_message(queue_url, receipt_handle)
+        receipt_handle = response['Messages'][0]['ReceiptHandle']
+        #print(f"Receipt Handle: {receipt_handle}")
+        delete_message(queue_url, receipt_handle)
 
-        print(f"{response}")
+        #print(f"{response}")
 
         # print the MessageAttributes:
-#         print(f"MessageAttributes: {response['Messages'][0]['MessageAttributes']}")
+        #print(f"MessageAttributes: {response['Messages'][0]['MessageAttributes']}")
 
         # print the order_no:
-#         print(f"Order No: {response['Messages'][0]['MessageAttributes']['order_no']['StringValue']}")
+        print(f"Order No: {response['Messages'][0]['MessageAttributes']['order_no']['StringValue']}")
         return response['Messages'][0]
 
     except Exception as e:
